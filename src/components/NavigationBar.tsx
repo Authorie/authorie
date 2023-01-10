2da988a1f90f066dcfbac8ad86e1789e73bd345b
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-01-10 16:22:20 +0700

    change colors and sizes

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -10,7 +10,7 @@ import { PencilIcon } from "@heroicons/react/24/solid";
 
 const NavigationBar = () => {
   return (
-    <nav className="flex h-screen w-72 flex-col bg-white px-8 pt-10 pb-16 text-2xl shadow-xl ring-1 ring-gray-900/5">
+    <nav className="flex h-screen w-fit flex-col bg-white px-8 pt-10 pb-16 text-2xl shadow-xl ring-1 ring-gray-900/5">
       <a href="#">
         <Image
           src="/authorie_logo.png"
@@ -22,57 +22,58 @@ const NavigationBar = () => {
       </a>
       <ul className="mt-20 flex flex-col space-y-6 px-2">
         <li className="rounded-full py-4 hover:bg-gray-200">
-          <a href="#" className="flex items-center justify-start gap-6 pl-3">
+          <a href="#" className="flex items-center justify-start gap-6 px-3">
             <Image
               src="https://thumbs.dreamstime.com/b/cute-cat-portrait-square-photo-beautiful-white-closeup-105311158.jpg"
               alt="profile picture"
-              width="30"
-              height="30"
+              width="40"
+              height="40"
               className="rounded-full"
             />
             <span>nongfameza</span>
           </a>
         </li>
         <li className="rounded-full py-4 hover:bg-gray-200">
-          <a href="#" className="flex items-center justify-start gap-6 pl-3">
-            <HomeIcon width="30" height="30" />
+          <a href="#" className="flex items-center justify-start gap-6 px-3">
+            <HomeIcon width="40" height="40" />
             <span>Home</span>
           </a>
         </li>
         <li className="rounded-full py-4 hover:bg-gray-200">
-          <a href="#" className="flex items-center justify-start gap-6 pl-3">
-            <HeartIcon width="30" height="30" />
+          <a href="#" className="flex items-center justify-start gap-6 px-3">
+            <HeartIcon width="40" height="40" />
             <span>Favorites</span>
           </a>
         </li>
         <li className="rounded-full py-4 hover:bg-gray-200">
-          <a href="#" className="flex items-center justify-start gap-6 pl-3">
-            <BellIcon width="30" height="30" />
+          <a href="#" className="flex items-center justify-start gap-6 px-3">
+            <BellIcon width="40" height="40" />
             <span>Notification</span>
           </a>
         </li>
         <li className="rounded-full py-4 hover:bg-gray-200">
-          <a href="#" className="flex items-center justify-start gap-6 pl-3">
-            <ChatBubbleOvalLeftEllipsisIcon width="30" height="30" />
+          <a href="#" className="flex items-center justify-start gap-6 px-3">
+            <ChatBubbleOvalLeftEllipsisIcon width="40" height="40" />
             <span>Messages</span>
           </a>
         </li>
         <li className="rounded-full py-4 hover:bg-gray-200">
-          <a href="#" className="flex items-center justify-start gap-6 pl-3">
-            <MagnifyingGlassIcon width="30" height="30" />
+          <a href="#" className="flex items-center justify-start gap-6 px-3">
+            <MagnifyingGlassIcon width="40" height="40" />
             <span>Search</span>
           </a>
         </li>
         <li className="rounded-full py-4 hover:bg-gray-200">
           <a
             href="#"
-            className="flex items-center justify-start gap-6 pl-3 text-[#DCAE36]"
+            className="flex items-center justify-start gap-6 px-3 text-amber-400"
           >
             <Image
               src="/authorie_coin_logo.svg"
               alt="Authorie coin logo"
-              width="30"
-              height="30"
+              width="40"
+              height="40"
+              className="fill-amber-400"
             />
             <span>3120 Au</span>
           </a>
