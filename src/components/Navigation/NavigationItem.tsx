01780a078dea28dee4e60cc7fda5f46abb27d4e7
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-01-11 22:41:25 +0700

    add navigation side bar for nonuser

--- src/components/Navigation/NavigationItem.tsx
+++ src/components/Navigation/NavigationItem.tsx
@@ -3,14 +3,17 @@ import React from "react";
 interface Props {
   Icon: (props: { width: number; height: number }) => JSX.Element;
   title: string;
+  className?: string;
 }
 
-const NavigationItem = ({ Icon, title }: Props) => {
+const NavigationItem = ({ Icon, title, className }: Props) => {
   return (
     <li>
       <a
         href="#"
-        className="flex items-center justify-start gap-6 rounded-full p-4 hover:bg-gray-200"
+        className={`flex items-center justify-start gap-6 rounded-full p-4 hover:bg-gray-200 ${
+          className || ""
+        }`}
       >
         <Icon width={30} height={30} />
         <span className="hidden md:block">{title}</span>
--- src/components/Navigation/NavigationItemList.tsx
+++ src/components/Navigation/NavigationItemList.tsx
@@ -16,8 +16,59 @@ interface Props {
 }
 
 const NavigationItemList = ({ username, profileImage, coin }: Props) => {
+  if (username && profileImage && coin) {
+    return (
+      <ul className="mt-8 flex flex-col items-center gap-4 md:mt-12 md:items-stretch">
+        <li>
+          <a
+            href="#"
+            className="flex items-center justify-start gap-6 rounded-full p-4 hover:bg-gray-200"
+          >
+            <Image
+              src={profileImage}
+              alt="profile picture"
+              width="30"
+              height="30"
+              className="rounded-full"
+            />
+            <span className="hidden md:block">{username}</span>
+          </a>
+        </li>
+        <NavigationItem Icon={HomeIcon} title="Home" />
+        <NavigationItem Icon={HeartIcon} title="Favorites" />
+        <NavigationItem Icon={BellIcon} title="Notification" />
+        <NavigationItem
+          Icon={ChatBubbleOvalLeftEllipsisIcon}
+          title="Messages"
+        />
+        <NavigationItem Icon={MagnifyingGlassIcon} title="Search" />
+        <NavigationItem
+          Icon={({ width, height }) => (
+            <Image
+              src="/authorie_coin_logo.svg"
+              alt="Authorie coin logo"
+              width={width}
+              height={height}
+            />
+          )}
+          title={`${coin} Au`}
+          className="text-amber-500"
+        />
+        <li className="mt-2">
+          <a
+            href="#"
+            className="flex items-center justify-center gap-6 rounded-full bg-green-700 p-4 text-white hover:bg-green-800"
+          >
+            <PencilIcon width="24" height="24" />
+            <span className="hidden md:block">Create</span>
+          </a>
+        </li>
+      </ul>
+    );
+  }
+
   return (
-    <ul className="mt-8 flex flex-col items-center gap-4 md:mt-20 md:items-stretch">
+    <ul className="mt-8 flex flex-col items-center gap-4 md:mt-12 md:items-stretch">
       <li>
         <a
           href="#"
@@ -34,32 +85,22 @@ const NavigationItemList = ({ username, profileImage, coin }: Props) => {
         </a>
       </li>
       <NavigationItem Icon={HomeIcon} title="Home" />
-      <NavigationItem Icon={HeartIcon} title="Favorites" />
-      <NavigationItem Icon={BellIcon} title="Notification" />
-      <NavigationItem Icon={ChatBubbleOvalLeftEllipsisIcon} title="Messages" />
       <NavigationItem Icon={MagnifyingGlassIcon} title="Search" />
-      <li className="hidden md:block">
-        <a
-          href="#"
-          className="flex items-center justify-start gap-6 rounded-full p-4 text-amber-400 hover:bg-gray-200"
-        >
-          <Image
-            src="/authorie_coin_logo.svg"
-            alt="Authorie coin logo"
-            width="30"
-            height="30"
-            className="fill-amber-400"
-          />
-          <span className="hidden md:block">{coin} Au</span>
-        </a>
-      </li>
       <li className="mt-2">
         <a
           href="#"
-          className="flex items-center justify-center gap-6 rounded-full bg-green-600 p-4 text-white hover:bg-green-700"
+          className="flex items-center justify-center gap-6 rounded-full bg-gray-500 p-4 text-white hover:bg-gray-600"
+        >
+          <span className="hidden md:block">Register</span>
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          className="flex items-center justify-center gap-6 rounded-full bg-green-700 p-4 text-white hover:bg-green-800"
         >
           <PencilIcon width="24" height="24" />
-          <span className="hidden md:block">Create</span>
+          <span className="hidden md:block">Login</span>
         </a>
       </li>
     </ul>
