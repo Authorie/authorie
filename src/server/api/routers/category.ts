3cceb0994f5510e22dc99c2cd3805caaa4e12fcc
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-02-13 21:49:39 +0700

    add getFollowedCategories and followCategory endpoints

--- src/server/api/routers/category.ts
+++ src/server/api/routers/category.ts
@@ -1,11 +1,38 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
 export const categoryRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.category.findMany();
   }),
+  getFollowedCategories: protectedProcedure.query(async ({ ctx }) => {
+    return ctx.prisma.category.findMany({
+      where: {
+        users: {
+          some: {
+            userId: ctx.session.user.id,
+          },
+        },
+      },
+    });
+  }),
+  followCategory: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      ctx.prisma.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          categories: {
+            create: {
+              categoryId: input,
+            },
+          },
+        },
+      });
+    }),
   create: publicProcedure.input(z.string()).mutation(async ({ ctx, input }) => {
     const category = await ctx.prisma.category.create({
       data: {
