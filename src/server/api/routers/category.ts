f0b7f0c8b2d1ba6c8aa775b8e48f7e62fcd9c7da
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-02-25 20:41:43 +0700

    update error handling

--- src/server/api/routers/category.ts
+++ src/server/api/routers/category.ts
@@ -1,4 +1,6 @@
+import { Prisma } from "@prisma/client";
 import { z } from "zod";
+import { TRPCError } from "@trpc/server";
 
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
@@ -31,35 +33,70 @@ export const categoryRouter = createTRPCRouter({
     });
     return result.map((res) => res.category);
   }),
-  follow: protectedProcedure.input(z.string()).mutation(({ ctx, input }) => {
-    return ctx.prisma.user.update({
-      where: {
-        id: ctx.session.user.id,
-      },
-      data: {
-        categories: {
-          create: {
+  follow: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.categoriesOnUsers.create({
+          data: {
+            userId: ctx.session.user.id,
             categoryId: input,
           },
-        },
-      },
-    });
-  }),
-  unfollow: protectedProcedure.input(z.string()).mutation(({ ctx, input }) => {
-    return ctx.prisma.user.update({
-      where: {
-        id: ctx.session.user.id,
-      },
-      data: {
-        categories: {
-          delete: {
+        });
+      } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+          if (e.code === "P2002") {
+            throw new TRPCError({
+              code: "BAD_REQUEST",
+              message: `you have already followed this category: ${input}`,
+              cause: e,
+            });
+          } else if (e.code === "P2003") {
+            throw new TRPCError({
+              code: "BAD_REQUEST",
+              message: `user / category does not exist: category: ${input}, user: ${ctx.session.user.id}`,
+              cause: e,
+            });
+          }
+        }
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          cause: e,
+        });
+      }
+    }),
+  unfollow: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.categoriesOnUsers.delete({
+          where: {
             categoryId_userId: {
               userId: ctx.session.user.id,
               categoryId: input,
             },
           },
-        },
-      },
-    });
-  }),
+        });
+      } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+          if (e.code === "P2001") {
+            throw new TRPCError({
+              code: "BAD_REQUEST",
+              message: `you have already unfollowed this category: ${input}`,
+              cause: e,
+            });
+          } else if (e.code === "P2003") {
+            throw new TRPCError({
+              code: "BAD_REQUEST",
+              message: `user / category does not exist: category: ${input}, user: ${ctx.session.user.id}`,
+              cause: e,
+            });
+          }
+        }
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          cause: e,
+        });
+      }
+    }),
 });
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -6,23 +6,41 @@ import { TRPCError } from "@trpc/server";
 
 export const userRouter = createTRPCRouter({
   getData: publicProcedure.input(z.string()).query(async ({ ctx, input }) => {
-    return ctx.prisma.user.findUnique({
-      where: {
-        penname: input,
-      },
-      select: {
-        id: true,
-        penname: true,
-        image: true,
-        coin: true,
-        _count: {
-          select: {
-            followers: true,
-            following: true,
+    try {
+      const user = await ctx.prisma.user.findUniqueOrThrow({
+        where: {
+          penname: input,
+        },
+        select: {
+          id: true,
+          penname: true,
+          image: true,
+          coin: true,
+          _count: {
+            select: {
+              followers: true,
+              following: true,
+            },
           },
         },
-      },
-    });
+      });
+      return user;
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === "P2005") {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: `user does not exist: ${input}`,
+            cause: e,
+          });
+        }
+      }
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "something went wrong",
+        cause: e,
+      });
+    }
   }),
   getFollowing: publicProcedure
     .input(
@@ -33,7 +51,7 @@ export const userRouter = createTRPCRouter({
       })
     )
     .query(async ({ ctx, input }) => {
-      return ctx.prisma.user.findMany({
+      const following = await ctx.prisma.user.findMany({
         where: {
           NOT: {
             penname: null,
@@ -51,6 +69,7 @@ export const userRouter = createTRPCRouter({
           penname: input.cursor,
         },
       });
+      return following;
     }),
   getFollowers: publicProcedure
     .input(
@@ -61,7 +80,7 @@ export const userRouter = createTRPCRouter({
       })
     )
     .query(async ({ ctx, input }) => {
-      return ctx.prisma.user.findMany({
+      const followers = await ctx.prisma.user.findMany({
         where: {
           NOT: {
             penname: null,
@@ -79,6 +98,7 @@ export const userRouter = createTRPCRouter({
           penname: input.cursor,
         },
       });
+      return followers;
     }),
   update: protectedProcedure
     .input(
@@ -88,13 +108,12 @@ export const userRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       try {
-        const user = await ctx.prisma.user.update({
+        await ctx.prisma.user.update({
           where: {
             id: ctx.session.user.id,
           },
           data: input,
         });
-        return user;
       } catch (e) {
         if (e instanceof Prisma.PrismaClientKnownRequestError) {
           if (e.code === "P2002" && input.penname) {
