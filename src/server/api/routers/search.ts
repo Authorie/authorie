473d57dc91a3f9535d0aa9c0a2bf980282412286
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-02-27 00:19:19 +0700

    update search api

--- src/server/api/routers/search.ts
+++ src/server/api/routers/search.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const searchRouter = createTRPCRouter({
-  search: publicProcedure
+  searchUsers: publicProcedure
     .input(
       z.object({
         search: z.string(),
@@ -14,16 +14,42 @@ export const searchRouter = createTRPCRouter({
     .query(async ({ ctx, input }) => {
       const users = await ctx.prisma.user.findMany({
         take: input.take,
-        cursor: {
-          penname: input.cursor,
-        },
+        cursor: input.cursor ? { id: input.cursor } : undefined,
         where: {
           penname: {
             contains: input.search,
           },
         },
       });
-
-      return { users };
+      return users;
+    }),
+  searchBooks: publicProcedure
+    .input(
+      z.object({
+        search: z.string(),
+        take: z.number().default(5),
+        cursor: z.string().optional(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const books = await ctx.prisma.book.findMany({
+        take: input.take,
+        cursor: input.cursor ? { id: input.cursor } : undefined,
+        where: {
+          OR: [
+            {
+              title: {
+                contains: input.search,
+              },
+            },
+            {
+              description: {
+                contains: input.search,
+              },
+            },
+          ],
+        },
+      });
+      return books;
     }),
 });
