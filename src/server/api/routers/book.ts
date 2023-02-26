372b1fe1b44826577f167393581e6963a77b13e7
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-02-26 17:10:21 +0700

    add getAll api

--- src/server/api/routers/book.ts
+++ src/server/api/routers/book.ts
@@ -1,10 +1,95 @@
-import { BookOwnerStatus, Prisma } from "@prisma/client";
+import { BookOwnerStatus, BookStatus, Prisma } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const bookRouter = createTRPCRouter({
+  getAll: publicProcedure
+    .input(
+      z.union([
+        z.object({
+          userId: z.string().uuid(),
+          cursor: z.string().optional(),
+          take: z.number().default(10),
+        }),
+        z.object({
+          status: z
+            .enum([
+              BookStatus.INITIAL,
+              BookStatus.DRAFT,
+              BookStatus.PUBLISHED,
+              BookStatus.ARCHIVED,
+            ])
+            .array()
+            .default([
+              BookStatus.INITIAL,
+              BookStatus.DRAFT,
+              BookStatus.PUBLISHED,
+            ]),
+          cursor: z.string().optional(),
+          take: z.number().default(10),
+        }),
+      ])
+    )
+    .query(async ({ ctx, input }) => {
+      if ("userId" in input) {
+        const { userId, cursor, take } = input;
+        const or: Prisma.BookWhereInput[] = [
+          {
+            status: {
+              in: [BookStatus.PUBLISHED, BookStatus.COMPLETED],
+            },
+            owners: {
+              some: {
+                userId: userId,
+              },
+            },
+          },
+        ];
+        if (ctx.session?.user) {
+          or.push({
+            status: {
+              in: [BookStatus.INITIAL, BookStatus.DRAFT],
+            },
+            owners: {
+              some: {
+                userId: {
+                  in: [ctx.session.user.id, userId],
+                },
+              },
+            },
+          });
+        }
+        const books = await ctx.prisma.book.findMany({
+          where: { OR: or },
+          cursor: cursor ? { id: cursor } : undefined,
+          take,
+        });
+        return books;
+      }
+      if (!ctx.session?.user) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "not authenticated",
+        });
+      }
+      const books = await ctx.prisma.book.findMany({
+        where: {
+          status: {
+            in: input.status,
+          },
+          owners: {
+            some: {
+              userId: ctx.session.user.id,
+            },
+          },
+        },
+        cursor: input.cursor ? { id: input.cursor } : undefined,
+        take: input.take,
+      });
+      return books;
+    }),
   create: protectedProcedure
     .input(
       z.object({
