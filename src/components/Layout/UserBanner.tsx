f64c86bd9ff56fd8af4eb333320f4f5d600a76e6
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-03-12 18:37:34 +0700

    refactor

--- src/components/Layout/UserBanner.tsx
+++ src/components/Layout/UserBanner.tsx
@@ -1,4 +1,3 @@
-import LoadingSpinner from "@components/ui/LoadingSpinner";
 import { PencilSquareIcon } from "@heroicons/react/24/outline";
 import { api } from "@utils/api";
 import { useSession } from "next-auth/react";
@@ -8,24 +7,17 @@ import { useCallback, useMemo, useState } from "react";
 
 type UserTab = "HOME" | "COMMUNITY" | "BOOK" | "ABOUT";
 
-export const parseUserTab = (pathname: string | undefined) => {
-  pathname = pathname?.toUpperCase();
-  switch (pathname) {
-    case "COMMUNITY":
-    case "BOOK":
-    case "ABOUT":
-      return pathname as UserTab;
-    default:
-      return "HOME" as UserTab;
-  }
-};
-
-const AuthorTab: { title: UserTab; url: string }[] = [
+const AuthorTab = [
   { title: "HOME", url: "" },
   { title: "COMMUNITY", url: "community" },
   { title: "BOOK", url: "book" },
   { title: "ABOUT", url: "about" },
-];
+] as const;
+
+const parseUserTab = (pathname: string | undefined) => {
+  const tab = AuthorTab.find((t) => pathname?.includes(t.url));
+  return tab || AuthorTab[0];
+};
 
 const getFollowedButtonClassName = (followed: boolean) => {
   if (followed) {
@@ -59,21 +51,18 @@ const UserBanner = ({ penname }: props) => {
     () => session?.user.penname === penname,
     [penname, session?.user.penname]
   );
-  const { data: user, isLoading: userIsLoading } = api.user.getData.useQuery(
-    penname,
-    {
-      enabled: penname != null,
-      onError() {
-        void router.push("/404");
-      },
-      onSuccess(data) {
-        if (data && data.penname) {
-          setUpdatedPenname(data.penname);
-          setUpdatedBio(data.bio);
-        }
-      },
-    }
-  );
+  const { data: user } = api.user.getData.useQuery(penname, {
+    enabled: penname != null,
+    onError() {
+      void router.push("/404");
+    },
+    onSuccess(data) {
+      if (data && data.penname) {
+        setUpdatedPenname(data.penname);
+        setUpdatedBio(data.bio);
+      }
+    },
+  });
   const { data: isFollowed, isLoading: queryLoading } =
     api.user.isFollowUser.useQuery(penname, {
       enabled: !isOwner,
@@ -111,120 +100,121 @@ const UserBanner = ({ penname }: props) => {
     setIsEdit((prev) => !prev);
   }, []);
   const onSaveHandler = useCallback(() => {
-    updateProfile.mutate({
-      penname: updatedPenname,
-      bio: updatedBio,
-    });
-    void router.replace(`/${updatedPenname}`);
-  }, [router, updateProfile, updatedBio, updatedPenname]);
+    updateProfile.mutate(
+      {
+        penname: updatedPenname,
+        bio: updatedBio,
+      },
+      {
+        onSuccess(data) {
+          if (data.penname) {
+            void router.replace(`${data.penname}/${tab.url}`);
+          }
+          setIsEdit(false);
+        },
+      }
+    );
+  }, [router, tab, updateProfile, updatedBio, updatedPenname]);
 
   return (
     <>
       <div className="relative h-80 min-w-full">
-        {userIsLoading ? (
-          <div className="flex items-center justify-center">
-            <LoadingSpinner />
-          </div>
-        ) : (
-          <>
-            <div className="absolute inset-0">
-              <Image src="/mockWallpaper.jpeg" layout="fill" alt="wallpaper" />
+        <div className="absolute inset-0">
+          <Image src="/mockWallpaper.jpeg" layout="fill" alt="wallpaper" />
+        </div>
+        <div className="ml-40 h-full max-w-xl bg-black/60 px-7 pt-7 backdrop-blur-lg">
+          <div className="flex justify-between">
+            <div className="relative mb-3 flex h-32 w-32 items-center justify-center overflow-hidden rounded-full border">
+              <Image
+                src={session?.user.image || "/placeholder_profile.png"}
+                alt="profile picture"
+                width="250"
+                height="250"
+                className="absolute z-0 opacity-70"
+              />
             </div>
-            <div className="ml-40 h-full max-w-xl bg-black/60 px-7 pt-7 backdrop-blur-lg">
-              <div className="flex justify-between">
-                <div className="relative mb-3 flex h-32 w-32 items-center justify-center overflow-hidden rounded-full border">
-                  <Image
-                    src={session?.user.image || "/placeholder_profile.png"}
-                    alt="profile picture"
-                    width="250"
-                    height="250"
-                    className="absolute z-0 opacity-70"
-                  />
-                </div>
-                <div>
-                  <div className="w-fit cursor-pointer">
-                    {isEdit ? (
-                      <div className="flex gap-3">
-                        <button
-                          onClick={toggleIsEditHandler}
-                          className="rounded-xl border-2 border-red-500 px-5 py-1 text-red-500 hover:border-red-700 hover:text-red-700"
-                        >
-                          cancel
-                        </button>
-                        <button
-                          onClick={onSaveHandler}
-                          className="rounded-xl border-2 border-green-500 px-5 py-1 text-green-500 hover:border-green-700 hover:text-green-700"
-                        >
-                          save
-                        </button>
-                      </div>
-                    ) : (
-                      <PencilSquareIcon
-                        width={25}
-                        height={25}
-                        onClick={toggleIsEditHandler}
-                        className="text-white hover:text-gray-500"
-                      />
-                    )}
+            <div>
+              <div className="w-fit cursor-pointer">
+                {isEdit ? (
+                  <div className="flex gap-3">
+                    <button
+                      onClick={toggleIsEditHandler}
+                      className="rounded-xl border-2 border-red-500 px-5 py-1 text-red-500 hover:border-red-700 hover:text-red-700"
+                    >
+                      cancel
+                    </button>
+                    <button
+                      onClick={onSaveHandler}
+                      className="rounded-xl border-2 border-green-500 px-5 py-1 text-green-500 hover:border-green-700 hover:text-green-700"
+                    >
+                      save
+                    </button>
                   </div>
-                </div>
-              </div>
-              <div className="mb-2 flex items-center justify-between">
-                {isEdit ? (
-                  <input
-                    placeholder={updatedPenname}
-                    className="bg-transparent text-2xl font-bold text-white placeholder-gray-400 outline-none focus:outline-none"
-                    onChange={(e) => setUpdatedPenname(e.target.value)}
-                    value={updatedPenname}
-                  />
                 ) : (
-                  <h2 className="text-2xl font-bold text-white">{penname}</h2>
-                )}
-                {status === "authenticated" && user?.id !== session.user.id && (
-                  <button
-                    type="button"
-                    onClick={followButtonOnClickHandler}
-                    disabled={
-                      queryLoading ||
-                      followUserMutation.isLoading ||
-                      unfollowUserMutation.isLoading
-                    }
-                    className={getFollowedButtonClassName(Boolean(isFollowed))}
-                  >
-                    {Boolean(isFollowed) ? "Followed" : "Follow"}
-                  </button>
-                )}
-              </div>
-              <div className="mb-3 flex gap-20 text-white">
-                <p>
-                  <span className="font-semibold">
-                    {user?._count.followers || 0}
-                  </span>{" "}
-                  followers
-                </p>
-                <p>
-                  <span className="font-semibold">
-                    {user?._count.following || 0}
-                  </span>{" "}
-                  following
-                </p>
-              </div>
-              <div className="max-h-24 w-4/5 text-sm text-gray-100">
-                {isEdit ? (
-                  <textarea
-                    rows={2}
-                    placeholder={user?.bio || "Put bio here"}
-                    value={updatedBio}
-                    onChange={(e) => setUpdatedBio(e.target.value)}
-                    className="w-full resize-none bg-transparent placeholder-gray-400 outline-none"
+                  <PencilSquareIcon
+                    width={25}
+                    height={25}
+                    onClick={toggleIsEditHandler}
+                    className="text-white hover:text-gray-500"
                   />
-                ) : (
-                  <p>{user?.bio || ""}</p>
                 )}
               </div>
             </div>
-          </>
-        )}
+          </div>
+          <div className="mb-2 flex items-center justify-between">
+            {isEdit ? (
+              <input
+                placeholder={updatedPenname}
+                className="bg-transparent text-2xl font-bold text-white placeholder-gray-400 outline-none focus:outline-none"
+                onChange={(e) => setUpdatedPenname(e.target.value)}
+                value={updatedPenname}
+              />
+            ) : (
+              <h2 className="text-2xl font-bold text-white">{penname}</h2>
+            )}
+            {status === "authenticated" && user?.id !== session.user.id && (
+              <button
+                type="button"
+                onClick={followButtonOnClickHandler}
+                disabled={
+                  queryLoading ||
+                  followUserMutation.isLoading ||
+                  unfollowUserMutation.isLoading
+                }
+                className={getFollowedButtonClassName(Boolean(isFollowed))}
+              >
+                {Boolean(isFollowed) ? "Followed" : "Follow"}
+              </button>
+            )}
+          </div>
+          <div className="mb-3 flex gap-20 text-white">
+            <p>
+              <span className="font-semibold">
+                {user?._count.followers || 0}
+              </span>{" "}
+              followers
+            </p>
+            <p>
+              <span className="font-semibold">
+                {user?._count.following || 0}
+              </span>{" "}
+              following
+            </p>
+          </div>
+          <div className="max-h-24 w-4/5 text-sm text-gray-100">
+            {isEdit ? (
+              <textarea
+                rows={2}
+                placeholder={user?.bio || "Put bio here"}
+                value={updatedBio}
+                onChange={(e) => setUpdatedBio(e.target.value)}
+                className="w-full resize-none bg-transparent placeholder-gray-400 outline-none"
+              />
+            ) : (
+              <p>{user?.bio || ""}</p>
+            )}
+          </div>
+        </div>
       </div>
       <div className="sticky top-0 z-40 min-w-full">
         <div className="ml-40 flex max-w-xl items-center justify-between bg-black/60 px-7 py-3 shadow-lg backdrop-blur-lg">
@@ -232,7 +222,7 @@ const UserBanner = ({ penname }: props) => {
             <button
               key={data.title}
               onClick={() => void router.push(`/${penname}/${data.url}`)}
-              className={getTabClassName(data.title, tab)}
+              className={getTabClassName(data.title, tab.title)}
             >
               {data.title}
             </button>
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -36,6 +36,11 @@ export const userRouter = createTRPCRouter({
             cause: e,
           });
         }
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "can't get user's data",
+          cause: e,
+        });
       } else {
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
@@ -206,7 +211,7 @@ export const userRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       try {
-        await ctx.prisma.user.update({
+        return await ctx.prisma.user.update({
           where: {
             id: ctx.session.user.id,
           },
