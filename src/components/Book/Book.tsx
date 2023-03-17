26e9362d4c9043e7d398086d96600870f93cfc2c
four58 <four.music@hotmail.com>
2023-03-18 02:35:32 +0700

    add favorite api and edit book page

--- src/components/Book/Book.tsx
+++ src/components/Book/Book.tsx
@@ -2,7 +2,7 @@ import { EyeIcon, HeartIcon, StarIcon } from "@heroicons/react/24/outline";
 import Image from "next/legacy/image";
 import { useRouter } from "next/router";
 import StarIconSolid from "@heroicons/react/24/solid/StarIcon";
-import { useState } from "react";
+import type { MouseEvent } from "react";
 import { api } from "@utils/api";
 
 type props = {
@@ -15,11 +15,34 @@ type props = {
 
 const Book = ({ title, description, read, like, id }: props) => {
   const router = useRouter();
-  const [favorite, setFavorite] = useState(false);
+  const utils = api.useContext();
+  const { data: isFavorite } = api.book.isFavorite.useQuery({ id: id });
+  const unfavoriteBook = api.book.unfavorite.useMutation({
+    onSuccess: async () => {
+      await utils.book.invalidate();
+    },
+  });
+  const favoriteBook = api.book.favorite.useMutation({
+    onSuccess: async () => {
+      await utils.book.invalidate();
+    },
+  });
 
   const onClickHandler = () => {
     const penname: string = router.query.penname as string;
     void router.push(`/${penname}/book/${id}`);
+  };
+
+  const addFavoriteHandler = (e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    favoriteBook.mutate({ id: id });
+    console.log("add");
+  };
+
+  const removeFavoriteHandler = (e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    unfavoriteBook.mutate({ id: id });
+    console.log("remove");
   };
 
   return (
@@ -28,13 +51,21 @@ const Book = ({ title, description, read, like, id }: props) => {
       className="flex cursor-pointer transition duration-100 ease-in-out hover:-translate-y-1 hover:scale-[1.01]"
     >
       <div className="h-72 w-3 rounded-r-lg bg-authGreen-600 shadow-lg" />
-      <div className="w-52 rounded-l-lg bg-white shadow-lg">
+      <div className="flex w-52 flex-col rounded-l-lg bg-white pb-2 shadow-lg">
         <div className="relative h-28 w-full overflow-hidden rounded-tl-lg">
           <Image src="/mockWallpaper.jpeg" alt="book picture" layout="fill" />
-          <StarIcon className="absolute bottom-0 right-0 h-10 w-10 text-yellow-400" />
-          <StarIconSolid className="absolute bottom-0 right-0 h-10 w-10 text-yellow-400" />
+          {!isFavorite && (
+            <button onClick={addFavoriteHandler}>
+              <StarIcon className="absolute bottom-0 right-0 h-10 w-10 text-yellow-400 hover:text-yellow-500" />
+            </button>
+          )}
+          {isFavorite && (
+            <button onClick={removeFavoriteHandler}>
+              <StarIconSolid className="absolute bottom-0 right-0 h-10 w-10 text-yellow-400 hover:text-yellow-500" />
+            </button>
+          )}
         </div>
-        <div className="px-2 pt-2">
+        <div className="flex grow flex-col justify-between px-2 pt-2">
           <div className="flex w-full flex-col justify-center gap-2">
             <h1 className="font-bold">{title}</h1>
             <p className="text-xs font-light line-clamp-6">{description}</p>
--- src/pages/[penname]/book/index.tsx
+++ src/pages/[penname]/book/index.tsx
@@ -15,9 +15,17 @@ const BookPage = () => {
           <Bars3CenterLeftIcon className="h-7 w-7 rounded-lg bg-dark-100 text-authGreen-600" />
           <MagnifyingGlassIcon className="h-7 w-7 rounded-lg bg-dark-100 text-authGreen-600" />
         </div>
-        <div className="grid grid-cols-4 gap-x-8 gap-y-6">
-          {books &&
-            books.items.map((data) => (
+        {books && books.items.length == 0 && (
+          <div className="flex flex-col items-center gap-4">
+            <p>You still don&apos;t have any book yet. Wanna create one?</p>
+            <button className="rounded-lg bg-green-600 px-3 py-2 text-white hover:bg-green-700">
+              Create book now!
+            </button>
+          </div>
+        )}
+        {books && (
+          <div className="grid grid-cols-4 gap-x-8 gap-y-6">
+            {books.items.map((data) => (
               <Book
                 key={data.id}
                 id={data.id}
@@ -27,7 +35,8 @@ const BookPage = () => {
                 read={100}
               />
             ))}
-        </div>
+          </div>
+        )}
       </div>
     </div>
   );
