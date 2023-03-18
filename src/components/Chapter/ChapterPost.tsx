8903ca16fec0a14c94b4ae5506fcd178b48e621e
four58 <four.music@hotmail.com>
2023-03-19 01:24:53 +0700

    connect api for chapter, like, comment

--- src/components/Chapter/ChapterPost.tsx
+++ src/components/Chapter/ChapterPost.tsx
@@ -1,33 +1,69 @@
 import ArrowTopRightOnSquareIcon from "@heroicons/react/24/outline/ArrowTopRightOnSquareIcon";
 import { useState } from "react";
-import LikeButton from "@components/Button/LikeButton";
+import Comment from "@components/Comment/Comment";
 import CommentInput from "@components/Comment/CommentInput";
-import CommentBoard from "@components/Comment/CommentBoard";
-import { comment } from "mocks/comment";
 import Image from "next/image";
-import CommentButton from "@components/Button/CommentButton";
+import { type RouterOutputs, api } from "@utils/api";
+import { useEditor, EditorContent } from "@tiptap/react";
+import type { Content } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { useSession } from "next-auth/react";
+import { LikeButton, CommentButton } from "@components/action";
 
 type props = {
-  bookTitle: string;
-  author: string[];
-  chapterNumber: number;
-  chapterTitle: string;
-  publishDate: Date;
-  content: string;
-  like: number;
-  numberOfComment: number;
+  chapter: RouterOutputs["chapter"]["getAll"]["items"][number];
 };
 
-const ChapterPost = ({
-  bookTitle,
-  chapterTitle,
-  publishDate,
-  content,
-  like,
-  author,
-  numberOfComment,
-}: props) => {
-  const [commentClicked, setCommentClicked] = useState(false);
+const ChapterPost = ({ chapter }: props) => {
+  const { status } = useSession();
+  const utils = api.useContext();
+  const [openComments, setOpenComments] = useState(false);
+  const editor = useEditor({
+    editable: false,
+    content: chapter.content as Content,
+    extensions: [StarterKit],
+  });
+  const {
+    data: comments,
+    isSuccess,
+    isLoading,
+  } = api.comment.getAll.useQuery(
+    { chapterId: chapter.id },
+    { enabled: openComments }
+  );
+  const { data: isLike } = api.comment.isLike.useQuery({ id: chapter.id });
+  const likeMutation = api.chapter.like.useMutation({
+    onMutate: async () => {
+      await utils.chapter.isLike.cancel();
+      const previousLike = utils.chapter.isLike.getData();
+      utils.chapter.isLike.setData({ id: chapter.id }, (old) => !old);
+      return { previousLike };
+    },
+    onSettled: () => {
+      void utils.book.invalidate();
+    },
+  });
+  const unlikeMutation = api.chapter.unlike.useMutation({
+    onMutate: async () => {
+      await utils.chapter.isLike.cancel();
+      const previousLike = utils.chapter.isLike.getData();
+      utils.chapter.isLike.setData({ id: chapter.id }, (old) => !old);
+      return { previousLike };
+    },
+    onSettled: () => {
+      void utils.book.invalidate();
+    },
+  });
+
+  const onLikeHandler = () => {
+    if (likeMutation.isLoading && unlikeMutation.isLoading) return;
+    if (isLike) {
+      unlikeMutation.mutate({ id: chapter.id });
+    } else {
+      likeMutation.mutate({ id: chapter.id });
+    }
+  };
+
   return (
     <div className="max-w-5xl overflow-hidden rounded-xl bg-white shadow-md">
       <div className="relative flex flex-col gap-1 px-8 py-4">
@@ -36,53 +72,53 @@ const ChapterPost = ({
           <Image src="/mockWallpaper.jpeg" alt="wallpaper" fill />
         </div>
         <div className="z-10">
-          <h1 className="my-1 text-2xl font-bold">{chapterTitle}</h1>
-          <h1 className="text-dark-400">{bookTitle}</h1>
+          <h1 className="my-1 text-2xl font-bold">{chapter.title}</h1>
+          <h1 className="text-dark-400">{chapter.book.title}</h1>
           <p className="text-sm text-dark-600">
-            Author:{" "}
-            {author.map((penname) => (
-              <span key={penname} className="font-semibold">
-                {penname}
-              </span>
-            ))}
+            Author:
+            <span className="font-semibold">{chapter.owner.penname}</span>
           </p>
         </div>
       </div>
       <div className="my-3 px-8">
         <p className="mb-2 text-xs text-dark-400">
-          publish: {publishDate.toDateString()}
+          publish: {chapter.publishedAt.toDateString()}
         </p>
-        {content}
+        <EditorContent editor={editor} />
       </div>
       <div className="flex items-center justify-between px-8 py-2">
-        <LikeButton numberOfLike={like} width={6} height={6} textSize="sm" />
+        <LikeButton
+          isAuthenticated={status === "authenticated"}
+          isLike={Boolean(isLike)}
+          numberOfLike={chapter._count.likes}
+          onClickHandler={onLikeHandler}
+        />
         <CommentButton
-          onClick={() => setCommentClicked(() => !commentClicked)}
-          width={6}
-          height={6}
-          textSize="sm"
-          numberOfComment={numberOfComment}
+          numberOfComments={chapter._count.comments}
+          onClickHandler={() => setOpenComments((prev) => !prev)}
         />
         <div className="flex cursor-pointer items-center gap-2 rounded-full px-3 py-2 hover:bg-slate-100">
           <ArrowTopRightOnSquareIcon className="h-6 w-6" />
         </div>
       </div>
-      {commentClicked && (
+      {openComments && (
         <div className="bg-gray-300 px-4 pb-4 pt-2">
-          <CommentInput />
-          {comment &&
-            comment.map((comment) => (
-              <CommentBoard
-                key={comment.id}
-                penname={comment.penname}
-                comment={comment.comment}
-                image={comment.image}
-                commentImage={comment.commentImage}
-                like={comment.like}
-                commentNumber={comment.commentNumber}
-                replyData={comment.replyData}
-              />
+          {status === "authenticated" && (
+            <CommentInput chapterId={chapter.id} />
+          )}
+          {isSuccess &&
+            comments.map((comment) => (
+              <Comment key={comment.id} comment={comment} />
             ))}
+          {isLoading && (
+            <div className="bg-indigo-500">
+              <svg
+                className="... mr-3 h-5 w-5 animate-spin"
+                viewBox="0 0 24 24"
+              ></svg>
+              Processing...
+            </div>
+          )}
         </div>
       )}
     </div>
---
+++ src/components/Comment/Comment.tsx
@@ -0,0 +1,116 @@
+import Image from "next/image";
+import EllipsisHorizontalIcon from "@heroicons/react/24/solid/EllipsisHorizontalIcon";
+
+import type { RouterOutputs } from "@utils/api";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { api } from "@utils/api";
+import CommentInput from "./CommentInput";
+import { LikeButton, CommentButton } from "@components/action";
+
+type props = {
+  comment:
+    | RouterOutputs["comment"]["getAll"][number]
+    | RouterOutputs["comment"]["getAll"][number]["replies"][number];
+};
+
+const Comment = ({ comment }: props) => {
+  const { status } = useSession();
+  const utils = api.useContext();
+  const [openReplies, setOpenReplies] = useState(false);
+  const { data: isLike } = api.comment.isLike.useQuery({ id: comment.id });
+  const likeMutation = api.chapter.like.useMutation({
+    onMutate: async () => {
+      await utils.comment.isLike.cancel();
+      const previousLike = utils.comment.isLike.getData();
+      utils.comment.isLike.setData({ id: comment.id }, (old) => !old);
+      return { previousLike };
+    },
+    onSettled: () => {
+      void utils.book.invalidate();
+    },
+  });
+  const unlikeMutation = api.chapter.unlike.useMutation({
+    onMutate: async () => {
+      await utils.comment.isLike.cancel();
+      const previousLike = utils.comment.isLike.getData();
+      utils.comment.isLike.setData({ id: comment.id }, (old) => !old);
+      return { previousLike };
+    },
+    onSettled: () => {
+      void utils.book.invalidate();
+    },
+  });
+
+  const onLikeHandler = () => {
+    if (likeMutation.isLoading && unlikeMutation.isLoading) return;
+    if (isLike) {
+      unlikeMutation.mutate({ id: comment.id });
+    } else {
+      likeMutation.mutate({ id: comment.id });
+    }
+  };
+
+  return (
+    <div className="mt-3 rounded-xl bg-white py-1 pl-3 pr-1">
+      <div className="flex gap-3 ">
+        <div className="mt-4 h-8 w-8 overflow-hidden rounded-full">
+          <Image
+            src={comment.user.image || "/placeholder_profile.png"}
+            alt={`${comment.user.penname || "user"} profile image`}
+            width={50}
+            height={50}
+          />
+        </div>
+        <div className="flex grow flex-col">
+          <span className="font-semibold text-authGreen-600">
+            {comment.user.penname || ""}
+          </span>
+          <div className="flex rounded-lg bg-gray-200 px-4 py-1">
+            {comment.image !== null && (
+              <Image
+                src={comment.image}
+                alt={`${comment.user.penname || "user"} commment's image`}
+              />
+            )}
+            <div className="flex grow items-center justify-between">
+              <p className="text-sm">{comment.content}</p>
+              <EllipsisHorizontalIcon className="h-7 w-7" />
+            </div>
+          </div>
+          <div className="mt-1 flex items-center gap-7">
+            <LikeButton
+              isAuthenticated={status === "authenticated"}
+              isLike={Boolean(isLike)}
+              numberOfLike={comment._count.likes}
+              onClickHandler={onLikeHandler}
+              small
+            />
+            {comment.parentCommentId === null && (
+              <CommentButton
+                numberOfComments={comment._count.replies}
+                onClickHandler={() => setOpenReplies((prev) => !prev)}
+                small
+              />
+            )}
+            <p className="text-xs text-dark-400">date time</p>
+          </div>
+        </div>
+      </div>
+      {openReplies &&
+        "replies" in comment &&
+        comment.replies.map((reply) => (
+          <div key={comment.id} className="ml-10">
+            <Comment comment={reply} />
+          </div>
+        ))}
+      {openReplies && (
+        <div className="ml-10">
+          <CommentInput chapterId={comment.chapterId} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Comment;
