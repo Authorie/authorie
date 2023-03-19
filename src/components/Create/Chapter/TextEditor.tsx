69412401b4e7531e92c9072a9822bff5e47bc1bb
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-03-19 15:48:18 +0700

    connect createChapter page with api

--- src/components/Create/Chapter/TextEditor.tsx
+++ src/components/Create/Chapter/TextEditor.tsx
@@ -1,43 +1,44 @@
-import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
-import Underline from '@tiptap/extension-underline'
-import Highlight from '@tiptap/extension-highlight'
-import TextStyle from '@tiptap/extension-text-style'
-import Color from '@tiptap/extension-color'
-import Link from '@tiptap/extension-link'
-import Table from '@tiptap/extension-table'
-import TableCell from '@tiptap/extension-table-cell'
-import TableHeader from '@tiptap/extension-table-header'
-import TableRow from '@tiptap/extension-table-row'
-import CharacterCount from '@tiptap/extension-character-count'
-import Image from '@tiptap/extension-image'
+import CharacterCount from "@tiptap/extension-character-count";
+import Color from "@tiptap/extension-color";
+import Highlight from "@tiptap/extension-highlight";
+import Image from "@tiptap/extension-image";
+import Link from "@tiptap/extension-link";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import TextStyle from "@tiptap/extension-text-style";
+import Underline from "@tiptap/extension-underline";
+import { EditorContent, useEditor, type JSONContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
 
-import { Heading } from './TextEditorMenu/Heading';
-import TextEditorMenuBar from './TextEditorMenu/TextEditorMenuBar';
+import { Heading } from "./TextEditorMenu/Heading";
+import TextEditorMenuBar from "./TextEditorMenu/TextEditorMenuBar";
 
-import { api } from '@utils/api'
+type props = {
+  onEditorUpdate: (content: JSONContent) => void;
+};
 
-const TextEditor = () => {
-
+const TextEditor = ({ onEditorUpdate }: props) => {
   const editor = useEditor({
     extensions: [
       StarterKit.configure({
         heading: false,
         paragraph: {
           HTMLAttributes: {
-            class: 'text-base'
-          }
+            class: "text-base",
+          },
         },
         bulletList: {
           HTMLAttributes: {
-            class: 'list-disc px-4'
-          }
+            class: "list-disc px-4",
+          },
         },
         orderedList: {
           HTMLAttributes: {
-            class: 'list-decimal px-4'
-          }
-        }
+            class: "list-decimal px-4",
+          },
+        },
       }),
       Underline,
       Heading,
@@ -46,52 +47,53 @@ const TextEditor = () => {
       Color,
       Link.configure({
         HTMLAttributes: {
-          class: 'rounded shadow-md bg-white p-1 hover:underline hover:bg-slate-100 text-blue-500'
+          class:
+            "rounded shadow-md bg-white p-1 hover:underline hover:bg-slate-100 text-blue-500",
         },
       }),
       Table.configure({
         resizable: true,
         HTMLAttributes: {
-          class: 'border-collapse m-0 select-all overflow-hidden w-full table-auto'
-        }
+          class:
+            "border-collapse m-0 select-all overflow-hidden w-full table-auto",
+        },
       }),
       TableRow.configure({
         HTMLAttributes: {
-          class: 'select-all'
-        }
+          class: "select-all",
+        },
       }),
       TableHeader.configure({
         HTMLAttributes: {
-          class: 'border-slate-500 border-2 border-solid bg-slate-200 relative text-left select-all'
-        }
+          class:
+            "border-slate-500 border-2 border-solid bg-slate-200 relative text-left select-all",
+        },
       }),
       TableCell.configure({
         HTMLAttributes: {
-          class: 'border-slate-500 border-2 border-solid w-20 text-left select-all'
-        }
+          class:
+            "border-slate-500 border-2 border-solid w-20 text-left select-all",
+        },
       }),
       Image,
       CharacterCount,
-
     ],
-    content: '',
+    content: "",
     editorProps: {
       attributes: {
-        class: 'focus:outline-none min-h-[500px] w-full px-5',
+        class: "focus:outline-none min-h-[500px] w-full px-5",
       },
     },
     onUpdate: ({ editor }) => {
-      const output = editor.getJSON()
-      console.log(output);
-      // send the content to an API here
+      onEditorUpdate(editor.getJSON());
     },
-    autofocus: 'start',
-  })
+    autofocus: "start",
+  });
 
   return (
     <div className="min-h-[500px]">
       {editor && <TextEditorMenuBar editor={editor}></TextEditorMenuBar>}
-      <EditorContent className='h-full w-full' editor={editor} />
+      <EditorContent className="h-full w-full" editor={editor} />
     </div>
   );
 };
--- src/pages/create/chapter.tsx
+++ src/pages/create/chapter.tsx
@@ -1,14 +1,33 @@
 import BookComboBox from "@components/Create/Chapter/BookComboBox";
-import ChapterDraftCard from "@components/Create/Chapter/ChapterDraftCard";
 import TextEditor from "@components/Create/Chapter/TextEditor";
 import { PhotoIcon } from "@heroicons/react/24/outline";
+import type { JSONContent } from "@tiptap/react";
+import type { RouterOutputs } from "@utils/api";
+import { api } from "@utils/api";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 
-const DraftChapter = [
-  { id: "1234", title: "chapter 1" },
-  { id: "12345", title: "chapter 2" },
-];
+type Books = RouterOutputs["book"]["getAll"]["items"];
 
 const CreateChapter = () => {
+  const { status } = useSession();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState<JSONContent>();
+  const [book, setBook] = useState<Books[number]>();
+  const { data: user } = api.user.getData.useQuery(undefined, {
+    enabled: status === "authenticated",
+  });
+  const createChapterMutation = api.chapter.create.useMutation();
+  const onPublishHandler = () => {
+    if (title !== "" && content && book) {
+      createChapterMutation.mutate({
+        title,
+        content,
+        bookId: book.id,
+      });
+    }
+  };
+
   return (
     <div className="flex gap-4 rounded-b-2xl bg-white px-3 py-5">
       <div className="flex w-1/4 flex-col gap-3 rounded-lg bg-gray-100 p-4 shadow-lg">
@@ -16,32 +35,40 @@ const CreateChapter = () => {
         <p className="text-xs">
           Select one of previous chapter drafts, or you can create a new one.
         </p>
-        {DraftChapter.map((chapter) => (
+        {/* {DraftChapter.map((chapter) => (
           <ChapterDraftCard
             key={chapter.id}
             id={chapter.id}
             title={chapter.title}
           />
-        ))}
+        ))} */}
       </div>
       <div className="w-3/4 overflow-hidden rounded-lg bg-gray-100 shadow-lg">
         <div className="flex flex-col bg-gray-200 px-4 py-3">
           <PhotoIcon className="h-6 w-6" />
           <input
             placeholder="Untitled"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
             className="my-2 bg-transparent text-2xl font-semibold placeholder-gray-400 outline-none focus:outline-none"
           />
           <div className="mb-2 flex items-center">
             <span className="mr-4 text-xs text-gray-600">Author: </span>
-            <span className="text-xs">author name</span>
+            {user && <span className="text-xs">{user.penname}</span>}
           </div>
-          <div className="flex items-center">
-            <span className="mr-4 text-xs text-gray-600">Book: </span>
-            <BookComboBox />
-          </div>
+          {user && (
+            <div className="flex items-center">
+              <span className="mr-4 text-xs text-gray-600">Book: </span>
+              <BookComboBox
+                user={user}
+                selectedBook={book}
+                onSelectBook={(book: Books[number]) => setBook(book)}
+              />
+            </div>
+          )}
         </div>
         <div className="h-full bg-white px-8 py-6">
-          <TextEditor />
+          <TextEditor onEditorUpdate={setContent} />
           <div className="flex justify-between">
             <button className="h-6 w-24 rounded-lg bg-red-500 text-sm text-white">
               Delete
@@ -50,7 +77,10 @@ const CreateChapter = () => {
               <button className="h-6 w-24 rounded-lg bg-authBlue-500 text-sm text-white">
                 Save
               </button>
-              <button className="h-6 w-24 rounded-lg bg-authGreen-600 text-sm font-semibold text-white">
+              <button
+                className="h-6 w-24 rounded-lg bg-authGreen-600 text-sm font-semibold text-white"
+                onClick={onPublishHandler}
+              >
                 Publish
               </button>
             </div>
