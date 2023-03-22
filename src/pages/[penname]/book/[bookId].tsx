248e4c0ec84546a8a71c88ef6e673f46a6428ae0
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-03-23 03:24:04 +0700

    add optimistic update

--- src/pages/[penname]/book/[bookId].tsx
+++ src/pages/[penname]/book/[bookId].tsx
@@ -1,42 +1,36 @@
-import { useState } from "react";
-import { StarIcon } from "@heroicons/react/24/outline";
-import {
-  StarIcon as StarIconSolid,
-  PhotoIcon,
-} from "@heroicons/react/24/solid";
 import ChapterCard from "@components/Chapter/ChapterCard";
+import { Popover } from "@headlessui/react";
 import {
   ChevronLeftIcon,
   MagnifyingGlassIcon,
-  PencilSquareIcon,
+  PencilSquareIcon, StarIcon
 } from "@heroicons/react/24/outline";
 import {
   Bars3CenterLeftIcon,
   EyeIcon,
-  HeartIcon,
+  HeartIcon, PhotoIcon, StarIcon as StarIconSolid
 } from "@heroicons/react/24/solid";
-import Image from "next/image";
-import { useRouter } from "next/router";
+import { zodResolver } from "@hookform/resolvers/zod";
+import useImageUpload from "@hooks/imageUpload";
+import type { Category } from "@prisma/client";
+import { appRouter } from "@server/api/root";
+import { createInnerTRPCContext } from "@server/api/trpc";
+import { getServerAuthSession } from "@server/auth";
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import { api } from "@utils/api";
 import type {
   GetServerSidePropsContext,
-  InferGetServerSidePropsType,
+  InferGetServerSidePropsType
 } from "next";
-import { getServerAuthSession } from "@server/auth";
-import { createProxySSGHelpers } from "@trpc/react-query/ssg";
-import { createInnerTRPCContext } from "@server/api/trpc";
-import { appRouter } from "@server/api/root";
-import superjson from "superjson";
-import { useMemo } from "react";
+import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useMemo, useState } from "react";
 import { useForm } from "react-hook-form";
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import useImageUpload from "@hooks/imageUpload";
-import { Popover } from "@headlessui/react";
-import type { Category } from "@prisma/client";
-import { ToastContainer, toast } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
+import superjson from "superjson";
+import * as z from "zod";
 
 const validationSchema = z.object({
   title: z
@@ -73,15 +67,15 @@ type props = InferGetServerSidePropsType<typeof getServerSideProps>;
 
 const BookContent = ({ bookId }: props) => {
   const router = useRouter();
+  const utils = api.useContext();
+  const [isEdit, setIsEdit] = useState(false);
   const { data: categories } = api.category.getAll.useQuery();
   const { data: book } = api.book.getData.useQuery({
     id: bookId,
   });
-  const [addedCategories, setAddedCategories] = useState<Category[]>(
+  const [addedCategories, setAddedCategories] = useState(
     book?.categories.map((data) => data.category) || []
-  ); // tanstack-query as react state manager
-  const [isEdit, setIsEdit] = useState(false);
-  const utils = api.useContext();
+  );
   const { data: isFavorite } = api.book.isFavorite.useQuery({ id: bookId });
   const {
     imageData: bookCover,
@@ -97,7 +91,7 @@ const BookContent = ({ bookId }: props) => {
     onMutate: async () => {
       await utils.book.isFavorite.cancel();
       const previousFavorite = utils.book.isFavorite.getData();
-      utils.book.isFavorite.setData({ id: bookId }, (old) => !old);
+      utils.book.isFavorite.setData({ id: bookId }, false);
       return { previousFavorite };
     },
     onSettled: () => {
@@ -108,7 +102,7 @@ const BookContent = ({ bookId }: props) => {
     onMutate: async () => {
       await utils.book.isFavorite.cancel();
       const previousFavorite = utils.book.isFavorite.getData();
-      utils.book.isFavorite.setData({ id: bookId }, (old) => !old);
+      utils.book.isFavorite.setData({ id: bookId }, true);
       return { previousFavorite };
     },
     onSettled: () => {
@@ -143,6 +137,13 @@ const BookContent = ({ bookId }: props) => {
     }
   }, [book]);
 
+  const toggleCategoryHandler = (category: Category) => {
+    if (addedCategories.includes(category)) {
+      setAddedCategories(addedCategories.filter((data) => data !== category));
+    } else {
+      setAddedCategories([...addedCategories, category]);
+    }
+  }
   const toggleFavoriteHandler = () => {
     if (isFavorite) {
       unfavoriteBook.mutate({ id: bookId });
@@ -151,11 +152,27 @@ const BookContent = ({ bookId }: props) => {
     }
   };
   const updateBook = api.book.update.useMutation({
+    async onMutate(newBook) {
+      await utils.book.getData.cancel()
+      const prevData = utils.book.getData.getData({ id: newBook.id });
+      if (!prevData) return
+      const book = {
+        ...prevData,
+        title: newBook.title !== undefined ? newBook.title : prevData.title,
+        description: newBook.description !== undefined ? newBook.description : prevData.description,
+        categories: addedCategories.map((data) => ({ category: data })),
+        coverImage: newBook.coverImageUrl !== undefined ? newBook.coverImageUrl : prevData.coverImage,
+        wallpaperImage: newBook.wallpaperImageUrl !== undefined ? newBook.wallpaperImageUrl : prevData.wallpaperImage,
+      }
+      utils.book.getData.setData({ id: newBook.id }, book);
+      return { prevData }
+    },
+    onError(_, newPost, ctx) {
+      if (!ctx?.prevData) return
+      utils.book.getData.setData({ id: newPost.id }, ctx.prevData);
+    },
     onSuccess() {
-      void utils.book.invalidate();
-      setIsEdit(false);
-      resetBookCover();
-      resetBookWallpaper();
+      resetHandler();
     },
     onSettled: () => {
       void utils.book.invalidate();
@@ -177,15 +194,15 @@ const BookContent = ({ bookId }: props) => {
       const promises = [
         bookCover
           ? uploadImageUrl.mutateAsync({
-              title: `${data.title}'s book cover image`,
-              image: bookCover,
-            })
+            title: `${data.title}'s book cover image`,
+            image: bookCover,
+          })
           : undefined,
         bookWallpaper
           ? uploadImageUrl.mutateAsync({
-              title: `${data.title}'s book wallpaper`,
-              image: bookWallpaper,
-            })
+            title: `${data.title}'s book wallpaper`,
+            image: bookWallpaper,
+          })
           : undefined,
       ] as const;
       const [coverImageUrl, wallpaperImageUrl] = await Promise.all(promises);
@@ -291,12 +308,8 @@ const BookContent = ({ bookId }: props) => {
                         )}
                         {addedCategories.map((category) => (
                           <span
-                            onClick={() =>
-                              setAddedCategories((prev) =>
-                                prev.filter((c) => c.id !== category.id)
-                              )
-                            }
                             key={category.id}
+                            onClick={() => toggleCategoryHandler(category)}
                             className="cursor-pointer select-none whitespace-nowrap rounded-full bg-authYellow-500 px-2 py-0.5 text-xs text-white hover:bg-red-600"
                           >
                             {category.title}
@@ -314,14 +327,9 @@ const BookContent = ({ bookId }: props) => {
                                 )
                                 .map((category) => (
                                   <button
-                                    key={category.id}
                                     type="button"
-                                    onClick={() =>
-                                      setAddedCategories((prev) => [
-                                        ...prev,
-                                        category,
-                                      ])
-                                    }
+                                    key={category.id}
+                                    onClick={() => toggleCategoryHandler(category)}
                                     className="flex w-36 items-center justify-center rounded-lg bg-white p-2 text-xs font-bold shadow-md hover:bg-gray-300"
                                   >
                                     {category.title}
@@ -331,10 +339,10 @@ const BookContent = ({ bookId }: props) => {
                                 (category: Category) =>
                                   !addedCategories.includes(category)
                               ).length === 0 && (
-                                <p className="text-sm font-semibold">
-                                  No more categories left...
-                                </p>
-                              )}
+                                  <p className="text-sm font-semibold">
+                                    No more categories left...
+                                  </p>
+                                )}
                             </div>
                           </Popover.Panel>
                           <Popover.Button
@@ -390,9 +398,8 @@ const BookContent = ({ bookId }: props) => {
             <div className="flex flex-col">
               <div
                 className={`
-                ${
-                  isEdit ? "justify-end gap-2" : "justify-center"
-                } ${"flex h-52 w-3/5 flex-col gap-2"}`}
+                ${isEdit ? "justify-end gap-2" : "justify-center"
+                  } ${"flex h-52 w-3/5 flex-col gap-2"}`}
               >
                 <div className="flex gap-4">
                   {isEdit ? (
@@ -419,11 +426,10 @@ const BookContent = ({ bookId }: props) => {
                         />
                         <p
                           className={`${"text-xs"} 
-                          ${
-                            watch("title") && watch("title").length > 50
+                          ${watch("title") && watch("title").length > 50
                               ? "text-red-500"
                               : "text-black"
-                          }`}
+                            }`}
                         >
                           {watch("title") ? watch("title").length : 0}/50
                         </p>
@@ -444,11 +450,10 @@ const BookContent = ({ bookId }: props) => {
                   >
                     {!isEdit && (
                       <PencilSquareIcon
-                        className={`w-7 ${
-                          book.isOwner
-                            ? " rounded-lg p-1 text-gray-800 hover:bg-gray-400"
-                            : "hidden"
-                        }`}
+                        className={`w-7 ${book.isOwner
+                          ? " rounded-lg p-1 text-gray-800 hover:bg-gray-400"
+                          : "hidden"
+                          }`}
                       />
                     )}
                   </button>
@@ -482,11 +487,10 @@ const BookContent = ({ bookId }: props) => {
                       />
                       <p
                         className={`${"text-xs"} 
-                          ${
-                            watch("description") &&
+                          ${watch("description") &&
                             watch("description").length > 300
-                              ? "text-red-500"
-                              : "text-black"
+                            ? "text-red-500"
+                            : "text-black"
                           }`}
                       >
                         {watch("description") ? watch("description").length : 0}
