ad8db4cace9488151e2b0010dc0387deb947ff1d
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-03-25 02:10:06 +0700

    add deleteCollaborationInvite api

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -347,4 +347,71 @@ export const userRouter = createTRPCRouter({
         });
       }
     }),
+  deleteCollaborationInvite: protectedProcedure
+    .input(z.object({ bookId: z.string().cuid(), userId: z.string().cuid() }))
+    .mutation(async ({ ctx, input }) => {
+      const { bookId, userId } = input;
+
+      // check if the book exists
+      let book;
+      try {
+        book = await ctx.prisma.book.findUniqueOrThrow({
+          where: {
+            id: bookId,
+          },
+          include: {
+            owners: true,
+          },
+        });
+      } catch (err) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "book does not exist",
+          cause: err,
+        });
+      }
+
+      // check if the user is the owner of the book
+      if (book.owners.some((owner) => owner.userId === ctx.session.user.id)) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "you are not the owner of this book",
+        });
+      }
+
+      const userBookOwner = book.owners.find(
+        (owner) => owner.userId === userId
+      );
+      if (!userBookOwner) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "user is not a collaborator / invitee",
+        });
+      }
+      if (userBookOwner.status !== BookOwnerStatus.INVITEE) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "user is not an invitee",
+        });
+      }
+
+      // delete the book owner
+      try {
+        await ctx.prisma.bookOwner.delete({
+          where: {
+            bookId_userId: {
+              bookId,
+              userId,
+            },
+          },
+        });
+      } catch (err) {
+        console.error(err);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "something went wrong",
+          cause: err,
+        });
+      }
+    }),
 });
