372524e6008c7a7c1a7fa6cab487a6ad42cc9839
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-03-25 17:34:39 +0700

    add inviteBookCollaborator api

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -337,6 +337,89 @@ export const userRouter = createTRPCRouter({
         }
       }
     }),
+  inviteCollaborator: protectedProcedure
+    .input(z.object({ bookId: z.string().cuid(), userId: z.string().uuid() }))
+    .mutation(async ({ ctx, input }) => {
+      const { bookId, userId } = input;
+      // check if the book exists
+      let book;
+      try {
+        book = await ctx.prisma.book.findUniqueOrThrow({
+          where: {
+            id: bookId,
+          },
+          include: {
+            owners: true,
+          },
+        });
+      } catch (err) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "book does not exist",
+          cause: err,
+        });
+      }
+
+      // check if the caller is the owner of the book
+      const isOwner = book.owners.some(
+        (owner) => owner.userId === ctx.session.user.id
+      );
+      if (!isOwner) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "you are not the owner of the book",
+        });
+      }
+
+      // check if the user is already a collaborator
+      const isCollaborator = book.owners.some(
+        (owner) => owner.userId === userId
+      );
+      if (isCollaborator) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "user is already a collaborator",
+        });
+      }
+
+      // // check if the user has already been invited
+      // const isInvited = book.owners.some(
+      //   (owner) =>
+      //     owner.userId === userId && owner.status === BookOwnerStatus.INVITEE
+      // );
+
+      // check if the user exists
+      try {
+        await ctx.prisma.user.findUniqueOrThrow({
+          where: {
+            id: userId,
+          },
+        });
+      } catch (err) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "user does not exist",
+          cause: err,
+        });
+      }
+
+      // create the invite
+      try {
+        return await ctx.prisma.bookOwner.create({
+          data: {
+            bookId,
+            userId,
+            status: BookOwnerStatus.INVITEE,
+          },
+        });
+      } catch (err) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "something went wrong",
+          cause: err,
+        });
+      }
+    }),
   responseCollaborationInvite: protectedProcedure
     .input(z.object({ bookId: z.string().cuid(), accept: z.boolean() }))
     .mutation(async ({ ctx, input }) => {
