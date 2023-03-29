43a17eac575f09efa75e9e7ee142bf6af8ae82b3
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-03-29 17:16:45 +0700

    add isFollowingEachOther to searchUsers

--- src/server/api/routers/search/searchUsers.ts
+++ src/server/api/routers/search/searchUsers.ts
@@ -3,6 +3,36 @@ import { makePagination } from "@server/utils";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
+interface withFollowingFollower {
+  following: {
+    followingId: string;
+  }[];
+  followers: {
+    followerId: string;
+  }[];
+}
+
+type WithFollowingEachOther<T extends withFollowingFollower> = Omit<
+  T,
+  "following" | "followers"
+> & {
+  isFollowingEachOther: boolean;
+};
+
+export function computeIsFollowingEachOther<User extends withFollowingFollower>(
+  userId: string | undefined,
+  user: User
+): WithFollowingEachOther<User> {
+  return {
+    ...user,
+    following: undefined,
+    followers: undefined,
+    isFollowingEachOther:
+      user.followers.some((follower) => follower.followerId === userId) &&
+      user.following.some((following) => following.followingId === userId),
+  };
+}
+
 const searchUsers = publicProcedure
   .input(
     z.object({
@@ -14,23 +44,35 @@ const searchUsers = publicProcedure
   .query(async ({ ctx, input }) => {
     const { search, limit, cursor } = input;
     try {
-      const users = await ctx.prisma.user.findMany({
-        take: limit + 1,
-        cursor: cursor ? { id: cursor } : undefined,
-        where: {
-          penname: {
-            contains: search,
-          },
-        },
-        include: {
-          _count: {
-            select: {
-              followers: true,
-              following: true,
+      const users = (
+        await ctx.prisma.user.findMany({
+          take: limit + 1,
+          cursor: cursor ? { id: cursor } : undefined,
+          where: {
+            penname: {
+              contains: search,
             },
           },
-        },
-      });
+          include: {
+            followers: {
+              select: {
+                followerId: true,
+              },
+            },
+            following: {
+              select: {
+                followingId: true,
+              },
+            },
+            _count: {
+              select: {
+                followers: true,
+                following: true,
+              },
+            },
+          },
+        })
+      ).map((user) => computeIsFollowingEachOther(ctx.session?.user.id, user));
       return makePagination(users, limit);
     } catch (err) {
       throw new TRPCError({
