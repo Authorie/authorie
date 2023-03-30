92cb0a404e89307ca504c05526599bdae7e065d3
four58 <four.music@hotmail.com>
2023-03-30 08:59:52 +0700

    set change to archive page in book page

--- src/pages/[penname]/book/index.tsx
+++ src/pages/[penname]/book/index.tsx
@@ -3,6 +3,10 @@ import Book from "@components/Book/Book";
 //   Bars3CenterLeftIcon,
 //   MagnifyingGlassIcon,
 // } from "@heroicons/react/24/outline";
+import {
+  ArchiveBoxIcon,
+  ArrowUturnLeftIcon,
+} from "@heroicons/react/24/outline";
 import { BookStatus } from "@prisma/client";
 import { api } from "@utils/api";
 import Link from "next/link";
@@ -16,6 +20,7 @@ import { createInnerTRPCContext } from "@server/api/trpc";
 import { appRouter } from "@server/api/root";
 import superjson from "superjson";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
 
 export const getServerSideProps = async (
   context: GetServerSidePropsContext
@@ -44,11 +49,14 @@ type props = InferGetServerSidePropsType<typeof getServerSideProps>;
 
 const BookPage = ({ penname }: props) => {
   const { status } = useSession();
+  const [openArchive, setOpenArchive] = useState(false);
   const { data: user } = api.user.getData.useQuery(undefined, {
     enabled: status === "authenticated",
   });
   const { data: books } = api.book.getAll.useQuery({ penname });
-
+  const BookArchiveList = books?.items.filter(
+    (book) => book.status === BookStatus.ARCHIVED
+  ).length;
   return (
     <div className="mb-8 mt-6 min-w-[1024px]">
       <div className="max-h-full rounded-lg bg-white p-4 px-6 shadow-lg">
@@ -56,6 +64,25 @@ const BookPage = ({ penname }: props) => {
           <Bars3CenterLeftIcon className="h-7 w-7 rounded-lg bg-dark-100 text-authGreen-600" />
           <MagnifyingGlassIcon className="h-7 w-7 rounded-lg bg-dark-100 text-authGreen-600" />
         </div> */}
+        <div className="flex items-center justify-start">
+          {!openArchive ? (
+            <div
+              onClick={() => setOpenArchive(true)}
+              className="mb-3 flex cursor-pointer items-center justify-center gap-2 rounded-full bg-gray-400 px-4 py-1 text-sm font-semibold text-white hover:bg-gray-500"
+            >
+              <ArchiveBoxIcon className="h-5 w-5" />
+              <p>Your Archived</p>
+            </div>
+          ) : (
+            <div
+              onClick={() => setOpenArchive(false)}
+              className="mb-3 flex cursor-pointer items-center justify-center gap-2 rounded-full bg-authGreen-400 px-4 py-1 text-sm font-semibold text-white hover:bg-authGreen-500"
+            >
+              <ArrowUturnLeftIcon className="h-5 w-5" />
+              <p>Your Book Shelf</p>
+            </div>
+          )}
+        </div>
         {books &&
           books.items.length === 0 &&
           (user && user.penname === penname ? (
@@ -75,27 +102,53 @@ const BookPage = ({ penname }: props) => {
           ))}
         {books && (
           <div className="grid grid-cols-4 gap-x-8 gap-y-6">
-            {books.items.map(
-              (book) =>
-                book.status !== BookStatus.ARCHIVED && (
-                  <Book
-                    key={book.id}
-                    id={book.id}
-                    title={book.title}
-                    coverImage={book.coverImage}
-                    description={book.description}
-                    isOwner={book.isOwner}
-                    status={book.status}
-                    like={book.chapters.reduce(
-                      (acc, curr) => acc + curr._count.likes,
-                      0
-                    )}
-                    read={book.chapters.reduce(
-                      (acc, curr) => acc + curr.views,
-                      0
-                    )}
-                  />
-                )
+            {!openArchive ? (
+              books.items.map(
+                (book) =>
+                  book.status !== BookStatus.ARCHIVED && (
+                    <Book
+                      key={book.id}
+                      id={book.id}
+                      title={book.title}
+                      coverImage={book.coverImage}
+                      description={book.description}
+                      isOwner={book.isOwner}
+                      status={book.status}
+                      like={book.chapters.reduce(
+                        (acc, curr) => acc + curr._count.likes,
+                        0
+                      )}
+                      read={book.chapters.reduce(
+                        (acc, curr) => acc + curr.views,
+                        0
+                      )}
+                    />
+                  )
+              )
+            ) : book.status === BookStatus.ARCHIVED && BookArchiveList > 0 ? (
+              books.items.map((book) => (
+                <Book
+                  key={book.id}
+                  id={book.id}
+                  title={book.title}
+                  coverImage={book.coverImage}
+                  description={book.description}
+                  isOwner={book.isOwner}
+                  status={book.status}
+                  like={book.chapters.reduce(
+                    (acc, curr) => acc + curr._count.likes,
+                    0
+                  )}
+                  read={book.chapters.reduce(
+                    (acc, curr) => acc + curr.views,
+                    0
+                  )}
+                />
+              ))
+            ) : (
+              <div className="flex items-center justify-center text-xl font-bold">
+                <p>No book being archived</p>
+              </div>
             )}
           </div>
         )}
