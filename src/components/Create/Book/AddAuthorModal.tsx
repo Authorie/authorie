bdf3f7a5aa376c9a368e91573bfb89540e2e23cc
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-03-30 09:27:42 +0700

    eslint fix

--- src/components/Create/Book/AddAuthorModal.tsx
+++ src/components/Create/Book/AddAuthorModal.tsx
@@ -1,7 +1,5 @@
-import { users } from "mocks/users";
 import type { ChangeEvent } from "react";
 import { useState } from "react";
-import AuthorResult from "./AuthorResult";
 
 const AddAuthorModal = () => {
   const [authorPenName, setAuthorPenName] = useState("");
@@ -16,18 +14,18 @@ const AddAuthorModal = () => {
     <div className="flex w-fit flex-col items-start justify-center rounded-xl bg-gray-200 p-2 pt-0">
       <div className="max-h-52 overflow-y-scroll border-b-2 border-gray-300 pt-2">
         <div className="h-fit">
-          {users.map((user) => (
+          {/* {users.map((user) => (
             <AuthorResult
               key={user.userId}
               penname={user.penname}
               onClickHandler={() => console.log(`add ${user.penname}`)}
             />
-          ))}
+          ))} */}
         </div>
       </div>
       <input
         type="text"
-        className="focus:shadow-outline mt-2 w-72 rounded-lg bg-white py-2 px-3 text-xs focus:outline-none"
+        className="focus:shadow-outline mt-2 w-72 rounded-lg bg-white px-3 py-2 text-xs focus:outline-none"
         placeholder="Enter author's pen name..."
         onChange={onChangeHandler}
       />
