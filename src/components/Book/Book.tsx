6a3ad1faf08ba216aabcb9733a58df90501d9bd9
four58 <four.music@hotmail.com>
2023-04-02 10:17:27 +0700

    add landing page and change book style

--- src/components/Book/Book.tsx
+++ src/components/Book/Book.tsx
@@ -113,9 +113,14 @@ const Book = ({
       } flex`}
     >
       <div className="h-72 w-3 rounded-r-lg bg-authGreen-600 shadow-lg" />
-      <div className="relative flex w-52 flex-col rounded-l-lg bg-white pb-2 shadow-lg">
+      <div className="relative flex w-52 flex-col rounded-l-lg pb-2 shadow-lg">
         {isOwner && (
           <>
+            {coverImage ? (
+              <Image src={coverImage} alt="book picture" fill />
+            ) : (
+              <div className="h-full w-full bg-authGreen-400" />
+            )}
             <div
               className={`
               ${status === BookStatus.INITIAL ? "bg-gray-400" : ""} 
@@ -131,45 +136,37 @@ const Book = ({
             {status === BookStatus.DRAFT && (
               <button
                 onClick={(e) => void publishBookHandler(e)}
-                className="absolute right-2 top-2 z-20 rounded-full border border-white bg-green-600 px-2 py-1 text-xs text-white hover:bg-green-700"
+                className="absolute right-2 top-2 z-10 rounded-full border border-white bg-green-600 px-2 py-1 text-xs text-white hover:bg-green-700"
               >
                 Publish now
               </button>
             )}
             {status === BookStatus.ARCHIVED && (
-              <button className="absolute right-2 top-2 z-20 rounded-full border border-white bg-yellow-600 px-2 py-1 text-xs text-white hover:bg-yellow-700">
+              <button className="absolute right-2 top-2 z-10 rounded-full border border-white bg-yellow-600 px-2 py-1 text-xs text-white hover:bg-yellow-700">
                 Unarchive
               </button>
             )}
           </>
         )}
-        <div className="relative h-28 w-full overflow-hidden rounded-tl-lg">
-          {coverImage ? (
-            <Image src={coverImage} alt="book picture" fill />
-          ) : (
-            <div className="h-full w-full bg-authGreen-400" />
-          )}
-          {!isOwner && (
-            <button onClick={toggleFavoriteHandler}>
-              {isFavorite ? (
-                <HiOutlineStar className="absolute bottom-0 right-0 h-10 w-10 text-yellow-400 hover:text-yellow-500" />
-              ) : (
-                <HiStar className="absolute bottom-0 right-0 h-10 w-10 text-yellow-400 hover:text-yellow-500" />
-              )}
-            </button>
-          )}
-        </div>
-        <div className="flex grow flex-col justify-between px-2 pt-2">
-          <div className="flex w-full flex-col justify-center gap-2">
-            <h1 className="font-bold">{title}</h1>
-            <p className="line-clamp-6 text-xs font-light">{description}</p>
+        {!isOwner && (
+          <button onClick={toggleFavoriteHandler}>
+            {isFavorite ? (
+              <HiOutlineStar className="absolute right-0 top-0 h-10 w-10 text-yellow-400 hover:text-yellow-500" />
+            ) : (
+              <HiStar className="absolute right-0 top-0 h-10 w-10 text-yellow-400 hover:text-yellow-500" />
+            )}
+          </button>
+        )}
+        <div className="z-10 flex grow flex-col justify-between">
+          <div className="mt-10 flex w-full flex-col items-center justify-center gap-2 px-3">
+            <h1 className="rounded-full bg-white/80 px-2 font-bold">{title}</h1>
           </div>
-          <div className="flex justify-between">
-            <div className="mt-2 flex items-center gap-1">
+          <div className="ml-2 flex w-fit items-center gap-4 rounded-full bg-white px-2">
+            <div className="flex items-center gap-1">
               <HiOutlineEye className="h-5 w-5 text-authGreen-600" />
               <p className="text-xs font-medium text-authGreen-600">{read}</p>
             </div>
-            <div className="mt-2 flex items-center gap-1">
+            <div className="flex items-center gap-1">
               <HiOutlineHeart className="h-5 w-5 text-red-400" />
               <p className="text-xs font-medium text-red-400">{like}</p>
             </div>
