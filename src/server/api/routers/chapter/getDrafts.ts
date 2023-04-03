d6f3df9978e28469673d283d43371164dd51aed0
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-04-04 01:31:37 +0700

    add one hour buffer to getDrafts

--- src/server/api/routers/chapter/getDrafts.ts
+++ src/server/api/routers/chapter/getDrafts.ts
@@ -1,6 +1,14 @@
 import { protectedProcedure } from "@server/api/trpc";
 
 const getDraftChapters = protectedProcedure.query(async ({ ctx }) => {
+  const lastHour = new Date();
+  if (lastHour.getHours() === 0) {
+    lastHour.setHours(23);
+    lastHour.setDate(lastHour.getDate() - 1);
+  } else {
+    lastHour.setHours(lastHour.getHours() - 1);
+  }
+
   return await ctx.prisma.chapter.findMany({
     where: {
       ownerId: ctx.session.user.id,
@@ -10,7 +18,7 @@ const getDraftChapters = protectedProcedure.query(async ({ ctx }) => {
         },
         {
           publishedAt: {
-            gt: new Date(),
+            gt: lastHour,
           },
         },
       ],
