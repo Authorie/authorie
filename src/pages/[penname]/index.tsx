b31f6e3900dd57f5ed8d95eea9eea03737c03c72
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-04-09 14:31:03 +0700

    implement author home page

--- src/pages/[penname]/index.tsx
+++ src/pages/[penname]/index.tsx
@@ -1,5 +1,29 @@
+import { useRouter } from "next/router";
+import ChapterFeed from "~/components/Chapter/ChapterFeed";
+import { api } from "~/utils/api";
+
 const HomePage = () => {
-  return <div>Home</div>;
+  const router = useRouter();
+  const penname = router.query.penname as string;
+  const { data } = api.chapter.getAllChapters.useInfiniteQuery(
+    {
+      penname,
+      limit: 10,
+    },
+    {
+      getNextPageParam: (lastpage) => lastpage.nextCursor,
+      enabled: router.isReady,
+    }
+  );
+
+  return (
+    <div className="flex w-[1024px] flex-col gap-8 py-8">
+      {data &&
+        data.pages
+          .flatMap((page) => page.items)
+          .map((chapter) => <ChapterFeed key={chapter.id} chapter={chapter} />)}
+    </div>
+  );
 };
 
 export default HomePage;
