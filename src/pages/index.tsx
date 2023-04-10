fbaf9b0f3d3bd37e9e4282c09989e9a637241c99
four58 <four.music@hotmail.com>
2023-04-11 02:05:52 +0700

    finish infinite scroll on FeedPage and add ChapterFeedSkeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,16 @@ import { useFollowedCategories } from "~/hooks/followedCategories";
 import { useSelectedCategory } from "~/hooks/selectedCategory";
 import { useSelectedDate } from "~/hooks/selectedDate";
 import { api } from "~/utils/api";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+import ChapterFeedSkeleton from "~/components/Chapter/ChapterFeedSkeleton";
 const ChapterFeed = dynamic(() => import("~/components/Chapter/ChapterFeed"));
+
+interface MyScrollingElement extends HTMLElement {
+  scrollHeight: number;
+  scrollTop: number;
+  clientHeight: number;
+}
 
 const Home = () => {
   const selectedDate = useSelectedDate();
@@ -18,16 +27,42 @@ const Home = () => {
       : selectedCategories === "following"
       ? followedCategories.map((c) => c.id)
       : [selectedCategories.id];
-  const { data } = api.chapter.getFeeds.useInfiniteQuery(
-    {
-      limit: 10,
-      categoryIds: categoryIds,
-      publishedAt: selectedDate,
-    },
-    {
-      getNextPageParam: (lastpage) => lastpage.nextCursor,
-    }
-  );
+  const { data, fetchNextPage, isFetchingNextPage, hasNextPage } =
+    api.chapter.getFeeds.useInfiniteQuery(
+      {
+        limit: 10,
+        categoryIds: categoryIds,
+        publishedAt: selectedDate,
+      },
+      {
+        getNextPageParam: (lastpage) => lastpage.nextCursor,
+      }
+    );
+
+  useEffect(() => {
+    let fetching = false;
+    const handleScroll = (e: Event) => {
+      const target = e.target as EventTarget & {
+        scrollingElement: MyScrollingElement;
+      };
+      const { scrollHeight, scrollTop, clientHeight } = target.scrollingElement;
+      if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.2) {
+        fetching = true;
+        if (hasNextPage)
+          fetchNextPage()
+            .then(() => {
+              fetching = false;
+            })
+            .catch(() => {
+              toast.error("Something went wrong");
+            });
+      }
+    };
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, [fetchNextPage, hasNextPage]);
 
   return (
     <div className="flex flex-col px-10 py-4">
@@ -40,6 +75,11 @@ const Home = () => {
               <ChapterFeed key={chapter.id} chapter={chapter} />
             ))}
       </div>
+      {isFetchingNextPage && (
+        <div className="mt-8">
+          <ChapterFeedSkeleton />
+        </div>
+      )}
     </div>
   );
 };
