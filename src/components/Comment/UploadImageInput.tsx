4c6bc298168223ab7211bf755420844ca0947c8f
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-04-14 12:44:53 +0700

    update upload image input

--- src/components/Comment/UploadImageInput.tsx
+++ src/components/Comment/UploadImageInput.tsx
@@ -1,8 +1,7 @@
+import Image from "next/image";
+import z from "zod";
 import useImageUpload from "~/hooks/imageUpload";
 import { api } from "~/utils/api";
-import z from "zod";
-import Image from "next/image";
-import { useEffect, useState } from "react";
 
 type props = {
   onClick: (image: string) => void;
@@ -11,32 +10,23 @@ type props = {
 const UploadImageInput = ({ onClick }: props) => {
   const { imageData, uploadHandler, resetImageData } = useImageUpload();
   const uploadImage = api.upload.uploadImage.useMutation();
-  const [image, setImage] = useState<string | null>(null);
 
   const submitHandler = async () => {
-    let commentImageUrl;
     const urlParser = z.string().startsWith("data:image");
     if (imageData && urlParser.safeParse(imageData).success) {
-      commentImageUrl = await uploadImage.mutateAsync({
+      const commentImageUrl = await uploadImage.mutateAsync({
         image: imageData,
       });
+      onClick(commentImageUrl)
+      resetImageData();
     }
-    if (commentImageUrl) {
-      setImage(commentImageUrl);
-      onClick(commentImageUrl);
-    }
-    resetImageData();
   };
-
-  useEffect(() => {
-    setImage(imageData);
-  }, [imageData]);
 
   return (
     <div className="flex flex-col items-center justify-center gap-2 rounded-lg bg-white p-2">
       <div className="flex h-64 w-72 items-center justify-center overflow-auto border">
-        {image ? (
-          <Image src={image} alt="Comment Image" width={300} height={300} />
+        {imageData ? (
+          <Image src={imageData} alt="Comment Image" width={300} height={300} />
         ) : (
           <div className="flex items-center justify-center rounded-lg font-semibold">
             {`Image size < 1 mb`}
