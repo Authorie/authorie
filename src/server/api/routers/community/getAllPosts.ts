2a7bea6c95eb95ff7571857b34d50d7b9bc1c8bd
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2023-04-16 23:34:24 +0700

    fix getAllPosts (communityPosts)

--- src/server/api/routers/community/getAllPosts.ts
+++ src/server/api/routers/community/getAllPosts.ts
@@ -4,23 +4,23 @@ import { publicProcedure } from "~/server/api/trpc"
 
 const getAllPosts = publicProcedure.input(z.object({ penname: z.string().cuid() })).query(({ ctx, input }) => {
   const { penname } = input
-  return ctx.prisma.user.findFirstOrThrow({
-    where: { penname },
-    select: {
-      communityPosts: {
-        include: {
-          user: true,
+  return ctx.prisma.communityPost.findMany({
+    where: {
+      author: {
+        penname,
+      }
+    },
+    include: {
+      user: true,
+      children: true,
+      _count: {
+        select: {
+          likes: true,
           children: true,
-          _count: {
-            select: {
-              likes: true,
-              children: true,
-            }
-          }
         }
       }
     }
   })
 })
 
-export default getAllPosts 
+export default getAllPosts
