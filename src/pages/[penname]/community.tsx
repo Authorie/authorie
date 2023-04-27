65e4c3e01afebc8b0e62e7b84776d8de4455fb0b
four58 <four.music@hotmail.com>
2023-04-27 23:28:58 +0700

    fix community loading page

--- src/pages/[penname]/community.tsx
+++ src/pages/[penname]/community.tsx
@@ -10,9 +10,12 @@ const CommunityPage = () => {
   const router = useRouter();
   const { status } = useSession();
   const communityPenname = router.query.penname as string;
-  const { data: user } = api.user.getData.useQuery(undefined, {
-    enabled: status === "authenticated",
-  });
+  const { data: user, isInitialLoading } = api.user.getData.useQuery(
+    undefined,
+    {
+      enabled: status === "authenticated",
+    }
+  );
   const { data: communityPostIds } = api.communityPosts.getAllPosts.useQuery(
     {
       penname: communityPenname,
@@ -30,15 +33,14 @@ const CommunityPage = () => {
   return (
     <div className="flex gap-5">
       <div className="my-8 flex w-fit flex-col items-start justify-start gap-7">
-        {user ? (
+        {user && (
           <CommunityInput
             penname={user.penname!}
             communityPenname={communityPenname}
             userImg={user.image ?? "/placeholder_profile.png"}
           />
-        ) : (
-          <CommunityInputSkeleton />
         )}
+        {isInitialLoading && <CommunityInputSkeleton />}
         {communityPosts.map(({ data: post }, index) =>
           post ? (
             <CommunityPost
@@ -49,6 +51,11 @@ const CommunityPage = () => {
           ) : (
             <CommunitySkeleton key={index} />
           )
+        )}
+        {communityPosts.length === 0 && (
+          <div className="flex h-96 w-[580px] items-center justify-center rounded-lg bg-white text-center text-lg font-semibold">
+            This community page still does not have any discussions.
+          </div>
         )}
       </div>
       <div className="sticky top-5 mt-8 flex h-96 w-[380px] items-center justify-center rounded-lg bg-white text-2xl font-bold">
